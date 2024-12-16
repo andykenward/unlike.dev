b0c1a5549eeb3dc6163c18ad4a2b04636aaa5443
Andy Kenward <andy@unlike.dev>
2024-12-16 18:11:21 +0000

    upgrade astro 5

---
+++ www/src/content.config.ts
@@ -0,0 +1,49 @@
+import { defineCollection, z } from "astro:content";
+import { file, glob } from "astro/loaders";
+
+const clients = defineCollection({
+  loader: glob({ pattern: "**/*.md", base: "./src/content/clients" }),
+  schema: ({ image }) =>
+    z.object({
+      sortOrder: z.number(),
+      asset: image(),
+      href: z.string().url(),
+      title: z.string(),
+    }),
+});
+
+const social = defineCollection({
+  loader: glob({ pattern: "**/*.md", base: "./src/content/social" }),
+  schema: ({ image }) =>
+    z.object({
+      sortOrder: z.number(),
+      asset: image(),
+      href: z.string().url(),
+      title: z.string(),
+    }),
+});
+
+const tags = defineCollection({
+  loader: glob({ pattern: "**/*.md", base: "./src/content/tags" }),
+  schema: z.object({
+    title: z.string(),
+    href: z.string().url(),
+  }),
+});
+
+const openSource = defineCollection({
+  loader: glob({ pattern: "**/*.md", base: "./src/content/open-source" }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      asset: image(),
+      href: z.string().url(),
+    }),
+});
+
+export const collections = {
+  clients,
+  social,
+  tags,
+  "open-source": openSource,
+};
--- www/src/content/open-source/github-actions-cloudflare-pages.md
+++ www/src/content/open-source/github-actions-cloudflare-pages.md
@@ -1,4 +1,5 @@
 ---
 title: GitHub Action Cloudflare Pages
+asset: "../../assets/svgs/github-action-cloudflare-pages.svg"
 href: https://github.com/andykenward/github-actions-cloudflare-pages
 ---
