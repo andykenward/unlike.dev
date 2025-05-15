a8d51b44f5abd48126d89abc0e797252922e1df5
Andy Kenward <andy@unlike.dev>
2025-05-15 22:11:44 +0000

    chore: remove tailwind astro plugin

--- www/astro.config.js
+++ www/astro.config.js
@@ -1,10 +1,9 @@
 import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://unlike.dev",
-  integrations: [tailwind(), sitemap()],
+  integrations: [sitemap()],
   compressHTML: true,
 });
