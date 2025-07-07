f3204e91d81b1e999b81d93bc1221b017afa4d01
Andy Kenward <andy@unlike.dev>
2025-07-07 21:02:49 +0000

    fix: space

--- playwright.config.ts
+++ playwright.config.ts
@@ -81,7 +81,7 @@ export default defineConfig({
     ? undefined
     : {
         command: "pnpm --filter www preview",
-        url: " http://localhost:4321/",
+        url: "http://localhost:4321/",
         timeout: 120 * 1000,
         reuseExistingServer: !process.env.CI,
       },
