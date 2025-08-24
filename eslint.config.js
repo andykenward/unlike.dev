af87f92b687712c21b9fe25fa80e33870d404a20
Andy Kenward <andy@unlike.dev>
2025-08-24 18:30:03 +0000

    feat: eslint-plugin-playwright

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,17 @@
+// @ts-check
+
 import eslintPluginAstro from "eslint-plugin-astro";
+import eslintPluginPlaywright from "eslint-plugin-playwright";
 
 export default [
   ...eslintPluginAstro.configs.recommended,
   ...eslintPluginAstro.configs["flat/jsx-a11y-strict"],
+  {
+    ...eslintPluginPlaywright.configs["flat/recommended"],
+    files: ["tests/**/*.spec.ts"],
+    rules: {
+      ...eslintPluginPlaywright.configs["flat/recommended"].rules,
+      "playwright/no-conditional-expect": "error",
+    },
+  },
 ];
